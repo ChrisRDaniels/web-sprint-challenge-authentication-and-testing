e1f4f8d96a41630d58b8ed3e8d695e6e121594b8
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-07-17 18:35:29 -0600

    end points and auth to login functioning

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,55 @@
 const router = require('express').Router();
+const Users = require('../users/users-model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+// Register User
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+  Users.add(user)
+    .then((saved) => {
+      res.status(201).json(saved.username);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
 });
 
+// Login User
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = getJwt(user.username);
+        res.status(200).json({
+          token,
+          message: `Welcome ${user.username}!`,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid User Credentials' });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
 });
+
+function getJwt(username) {
+  const payload = {
+    username,
+  };
+
+  const secret = process.env.JWT_SECRET || 'keepitsecretkeepitsafe';
+
+  const options = {
+    expiresIn: '1h',
+  };
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,22 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  const secret = process.env.JWT_SECRET || 'keepitsecretkeepitsafe';
+
+  if (token) {
+    jwt.verify(token, secret, (err, decodedtoken) => {
+      if (err) {
+        res.status(401).json(err);
+      } else {
+        req.decodedJwt = decodedtoken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ Error: 'Invalid credentials provided.' });
+  }
 };
